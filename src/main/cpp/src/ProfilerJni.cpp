c3933c54195757b41cb143817f1750a74b0a7371
Jason Lowe <jlowe@nvidia.com>
2024-04-16 13:45:55 -0500

    wip

--- src/main/cpp/src/ProfilerJni.cpp
+++ src/main/cpp/src/ProfilerJni.cpp
@@ -156,10 +156,69 @@ void CUPTIAPI callback_handler(void*, CUpti_CallbackDomain domain,
   }
 }
 
+std::string activity_kind_to_string(CUpti_ActivityKind kind)
+{
+  switch (kind) {
+    case CUPTI_ACTIVITY_KIND_MEMCPY: return "CUPTI_ACTIVITY_KIND_MEMCPY";
+    case CUPTI_ACTIVITY_KIND_MEMSET: return "CUPTI_ACTIVITY_KIND_MEMSET";
+    case CUPTI_ACTIVITY_KIND_KERNEL: return "CUPTI_ACTIVITY_KIND_KERNEL";
+    case CUPTI_ACTIVITY_KIND_CONCURRENT_KERNEL: return "CUPTI_ACTIVITY_KIND_CONCURRENT_KERNEL";
+    case CUPTI_ACTIVITY_KIND_DRIVER: return "CPUTI_ACTIVITY_KIND_DRIVER";
+    case CUPTI_ACTIVITY_KIND_RUNTIME: return "CUPTI_ACTIVITY_KIND_RUNTIME";
+    case CUPTI_ACTIVITY_KIND_INTERNAL_LAUNCH_API: return "CUPTI_ACTIVITY_KIND_INTERNAL_LAUNCH_API";
+    case CUPTI_ACTIVITY_KIND_EVENT: return "CUPTI_ACTIVITY_KIND_EVENT";
+    case CUPTI_ACTIVITY_KIND_METRIC: return "CUPTI_ACTIVITY_KIND_METRIC";
+    case CUPTI_ACTIVITY_KIND_DEVICE: return "CUPTI_ACTIVITY_KIND_DEVICE";
+    case CUPTI_ACTIVITY_KIND_CONTEXT: return "CUPTI_ACTIVITY_KIND_CONTEXT";
+    case CUPTI_ACTIVITY_KIND_NAME: return "CUPTI_ACTIVITY_KIND_NAME";
+    case CUPTI_ACTIVITY_KIND_MARKER: return "CUPTI_ACTIVITY_KIND_MARKER";
+    case CUPTI_ACTIVITY_KIND_MARKER_DATA: return "CUPTI_ACTIVITY_KIND_MARKER_DATA";
+    case CUPTI_ACTIVITY_KIND_SOURCE_LOCATOR: return "CUPTI_ACTIVITY_KIND_SOURCE_LOCATOR";
+    case CUPTI_ACTIVITY_KIND_GLOBAL_ACCESS: return "CUPTI_ACTIVITY_KIND_GLOBAL_ACCESS";
+    case CUPTI_ACTIVITY_KIND_BRANCH: return "CUPTI_ACTIVITY_KIND_BRANCH";
+    case CUPTI_ACTIVITY_KIND_OVERHEAD: return "CUPTI_ACTIVITY_KIND_OVERHEAD";
+    case CUPTI_ACTIVITY_KIND_CDP_KERNEL: return "CUPTI_ACTIVITY_KIND_CDP_KERNEL";
+    case CUPTI_ACTIVITY_KIND_PREEMPTION: return "CUPTI_ACTIVITY_KIND_PREEMPTION";
+    case CUPTI_ACTIVITY_KIND_ENVIRONMENT: return "CUPTI_ACTIVITY_KIND_ENVIRONMENT";
+    case CUPTI_ACTIVITY_KIND_EVENT_INSTANCE: return "CUPTI_ACTIVITY_KIND_EVENT_INSTANCE";
+    case CUPTI_ACTIVITY_KIND_MEMCPY2: return "CUPTI_ACTIVITY_KIND_MEMCPY2";
+    case CUPTI_ACTIVITY_KIND_METRIC_INSTANCE: return "CUPTI_ACTIVITY_KIND_METRIC_INSTANCE";
+    case CUPTI_ACTIVITY_KIND_INSTRUCTION_EXECUTION: return "CUPTI_ACTIVITY_KIND_INSTRUCTION_EXECUTION";
+    case CUPTI_ACTIVITY_KIND_UNIFIED_MEMORY_COUNTER: return "CUPTI_ACTIVITY_KIND_UNIFIED_MEMORY_COUNTER";
+    case CUPTI_ACTIVITY_KIND_FUNCTION: return "CUPTI_ACTIVITY_KIND_FUNCTION";
+    case CUPTI_ACTIVITY_KIND_MODULE: return "CUPTI_ACTIVITY_KIND_MODULE";
+    case CUPTI_ACTIVITY_KIND_DEVICE_ATTRIBUTE: return "CUPTI_ACTIVITY_KIND_DEVICE_ATTRIBUTE";
+    case CUPTI_ACTIVITY_KIND_SHARED_ACCESS: return "CUPTI_ACTIVITY_KIND_SHARED_ACCESS";
+    case CUPTI_ACTIVITY_KIND_PC_SAMPLING: return "CUPTI_ACTIVITY_KIND_PC_SAMPLING";
+    case CUPTI_ACTIVITY_KIND_PC_SAMPLING_RECORD_INFO: return "CUPTI_ACTIVITY_KIND_PC_SAMPLING_RECORD_INFO";
+    case CUPTI_ACTIVITY_KIND_INSTRUCTION_CORRELATION: return "CUPTI_ACTIVITY_KIND_INSTRUCTION_CORRELATION";
+    case CUPTI_ACTIVITY_KIND_OPENACC_DATA: return "CUPTI_ACTIVITY_KIND_OPENACC_DATA";
+    case CUPTI_ACTIVITY_KIND_OPENACC_LAUNCH: return "CUPTI_ACTIVITY_KIND_OPENACC_LAUNCH";
+    case CUPTI_ACTIVITY_KIND_OPENACC_OTHER: return "CUPTI_ACTIVITY_KIND_OPENACC_OTHER";
+    case CUPTI_ACTIVITY_KIND_CUDA_EVENT: return "CUPTI_ACTIVITY_KIND_CUDA_EVENT";
+    case CUPTI_ACTIVITY_KIND_STREAM: return "CUPTI_ACTIVITY_KIND_STREAM";
+    case CUPTI_ACTIVITY_KIND_SYNCHRONIZATION: return "CUPTI_ACTIVITY_KIND_SYNCHRONIZATION";
+    case CUPTI_ACTIVITY_KIND_EXTERNAL_CORRELATION: return "CUPTI_ACTIVITY_KIND_EXTERNAL_CORRELATION";
+    case CUPTI_ACTIVITY_KIND_NVLINK: return "CUPTI_ACTIVITY_KIND_NVLINK";
+    case CUPTI_ACTIVITY_KIND_INSTANTANEOUS_EVENT: return "CUPTI_ACTIVITY_KIND_INSTANTANEOUS_EVENT";
+    case CUPTI_ACTIVITY_KIND_INSTANTANEOUS_EVENT_INSTANCE: return "CUPTI_ACTIVITY_KIND_INSTANTANEOUS_EVENT_INSTANCE";
+    case CUPTI_ACTIVITY_KIND_INSTANTANEOUS_METRIC: return "CUPTI_ACTIVITY_KIND_INSTANTANEOUS_METRIC";
+    case CUPTI_ACTIVITY_KIND_INSTANTANEOUS_METRIC_INSTANCE: return "CUPTI_ACTIVITY_KIND_INSTANTANEOUS_METRIC_INSTANCE";
+    case CUPTI_ACTIVITY_KIND_MEMORY: return "CUPTI_ACTIVITY_KIND_MEMORY";
+    case CUPTI_ACTIVITY_KIND_PCIE: return "CUPTI_ACTIVITY_KIND_PCIE";
+    case CUPTI_ACTIVITY_KIND_OPENMP: return "CUPTI_ACTIVITY_KIND_OPENMP";
+    case CUPTI_ACTIVITY_KIND_MEMORY2: return "CUPTI_ACTIVITY_KIND_MEMORY2";
+    case CUPTI_ACTIVITY_KIND_MEMORY_POOL: return "CUPTI_ACTIVITY_KIND_MEMORY_POOL";
+    case CUPTI_ACTIVITY_KIND_GRAPH_TRACE: return "CUPTI_ACTIVITY_KIND_GRAPH_TRACE";
+    case CUPTI_ACTIVITY_KIND_JIT: return "CUPTI_ACTIVITY_KIND_JIT";
+    default: return "UNKNOWN";
+  }
+}
+
 void CUPTIAPI buffer_requested_callback(uint8_t** buffer_ptr_ptr, size_t* size_ptr,
     size_t* max_num_records_ptr)
 {
-  // TODO: Use a ring of pre-allocated buffers
+  // TODO: Reuse completed buffers
   *max_num_records_ptr = 0;
   *size_ptr = BUFF_SIZE;
   auto rc = posix_memalign(reinterpret_cast<void**>(buffer_ptr_ptr), ALIGN_BYTES, *size_ptr);
@@ -171,19 +230,55 @@ void CUPTIAPI buffer_requested_callback(uint8_t** buffer_ptr_ptr, size_t* size_p
 }
 
 void CUPTIAPI buffer_completed_callback(CUcontext, uint32_t,
-    uint8_t* buffer_ptr, size_t buffer_size, size_t valid_size)
+    uint8_t* buffer, size_t buffer_size, size_t valid_size)
 {
   if (valid_size > 0) {
+    // TODO: Process buffer on a background thread
     std::cerr << "GOT A BUFFER OF DATA FROM CUPTI, SIZE: " << valid_size << std::endl;
+    CUpti_Activity* record_ptr = nullptr;
+    auto rc = cuptiActivityGetNextRecord(buffer, valid_size, &record_ptr);
+    while (rc == CUPTI_SUCCESS) {
+      std::cerr << "RECORD: " << activity_kind_to_string(record_ptr->kind) << std::endl;
+      switch (record_ptr->kind) {
+        case CUPTI_ACTIVITY_KIND_DRIVER:
+        {
+          auto api_record = reinterpret_cast<CUpti_ActivityAPI const*>(record_ptr);
+          char const* name = nullptr;
+          cuptiGetCallbackName(CUPTI_CB_DOMAIN_DRIVER_API, api_record->cbid, &name);
+          name = name ? name : "NULL";
+          std::cerr << "  NAME: " << name << " THREAD: " << api_record->threadId << std::endl;
+          break;
+        }
+        case CUPTI_ACTIVITY_KIND_DEVICE:
+        {
+          auto device_record = reinterpret_cast<CUpti_ActivityDevice4 const*>(record_ptr);
+          char const* name = device_record->name != nullptr ? device_record->name : "NULL";
+          std::cerr << "  " << activity_kind_to_string(device_record->kind) << " " << name << std::endl;
+          break;
+        }
+        case CUPTI_ACTIVITY_KIND_RUNTIME:
+        {
+          auto api_record = reinterpret_cast<CUpti_ActivityAPI const*>(record_ptr);
+          char const* name = nullptr;
+          cuptiGetCallbackName(CUPTI_CB_DOMAIN_RUNTIME_API, api_record->cbid, &name);
+          name = name ? name : "NULL";
+          std::cerr << "  NAME: " << name << " THREAD: " << api_record->threadId << std::endl;
+          break;
+        }
+        default:
+          break;
+      }
+      rc = cuptiActivityGetNextRecord(buffer, valid_size, &record_ptr);
+    }
   }
-  free(buffer_ptr);
+  free(buffer);
 }
 
 }
 
 extern "C" {
 
-JNIEXPORT void JNICALL Java_com_nvidia_spark_rapids_jni_ProfilerJni_nativeInit(JNIEnv* env, jclass)
+JNIEXPORT void JNICALL Java_com_nvidia_spark_rapids_jni_Profiler_nativeInit(JNIEnv* env, jclass)
 {
   try {
     State = new subscriber_state(env);
@@ -204,6 +299,7 @@ JNIEXPORT void JNICALL Java_com_nvidia_spark_rapids_jni_ProfilerJni_nativeInit(J
     rc = cuptiActivityRegisterCallbacks(buffer_requested_callback, buffer_completed_callback);
     check_cupti(rc, "Error registering activity buffer callbacks");
 
+    check_cupti(cuptiEnableDomain(1, State->subscriber_handle, CUPTI_CB_DOMAIN_NVTX), "Error enabling NVTX domain");
     check_cupti(cuptiActivityEnable(CUPTI_ACTIVITY_KIND_DEVICE), "Error enabling device activity");
     check_cupti(cuptiActivityEnable(CUPTI_ACTIVITY_KIND_CONTEXT), "Error enabling context activity");
     check_cupti(cuptiActivityEnable(CUPTI_ACTIVITY_KIND_DRIVER), "Error enabling driver activity");
@@ -218,7 +314,7 @@ JNIEXPORT void JNICALL Java_com_nvidia_spark_rapids_jni_ProfilerJni_nativeInit(J
   CATCH_STD(env, );
 }
 
-JNIEXPORT void JNICALL Java_com_nvidia_spark_rapids_jni_ProfilerJni_nativeShutdown(JNIEnv* env, jclass)
+JNIEXPORT void JNICALL Java_com_nvidia_spark_rapids_jni_Profiler_nativeShutdown(JNIEnv* env, jclass)
 {
   try {
     if (State != nullptr) {
