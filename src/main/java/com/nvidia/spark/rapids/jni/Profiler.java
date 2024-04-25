ce7e9a0b82d18bf4de25f0393d7b2dd5beb25115
Jason Lowe <jlowe@nvidia.com>
2024-04-25 11:26:33 -0500

    Back to static cupti

--- src/main/java/com/nvidia/spark/rapids/jni/Profiler.java
+++ src/main/java/com/nvidia/spark/rapids/jni/Profiler.java
@@ -31,7 +31,6 @@ public class Profiler {
     if (writer == null) {
       File libPath;
       try {
-        NativeDepsLoader.loadNativeDep("cupti", true);
         libPath = NativeDepsLoader.loadNativeDep("profilerjni", true);
       } catch (IOException e) {
         throw new RuntimeException("Error loading profiler library", e);
