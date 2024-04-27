766dde5c7aa1e400686bfacd1cf022af2de11f87
Jason Lowe <jlowe@nvidia.com>
2024-04-26 22:31:05 -0500

    converter header check

--- src/main/cpp/src/profiler/spark_rapids_profile_converter.cpp
+++ src/main/cpp/src/profiler/spark_rapids_profile_converter.cpp
@@ -18,23 +18,21 @@
 
 #include "profiler_generated.h"
 
+#include <fstream>
 #include <iostream>
 #include <optional>
+#include <sstream>
 #include <string>
 #include <vector>
-
-constexpr int MAIN_RESULT_SUCCESS = 0;
-constexpr int MAIN_RESULT_FAILURE = 1;
-constexpr int MAIN_RESULT_USAGE = 2;
 
 struct program_options {
   std::optional<std::string_view> output_path;
   bool help = false;
 };
 
-void print_usage(std::string_view prog_name)
+void print_usage()
 {
-  std::cout << prog_name << " [OPTION]... profilebin" << std::endl;
+  std::cout << "spark_rapids_profile_converter [OPTION]... profilebin" << std::endl;
   std::cout << R"(
 Converts the spark-rapids profile in profile.bin into other forms.
 
@@ -82,31 +80,82 @@ parse_options(std::vector<std::string_view> args)
   return std::make_pair(opts, std::vector<std::string_view>(argp, args.end()));
 }
 
+size_t read_flatbuffer_size(std::ifstream& in)
+{
+  flatbuffers::uoffset_t fb_size;
+  in.read(reinterpret_cast<char*>(&fb_size), sizeof(fb_size));
+  return flatbuffers::EndianScalar(fb_size);
+}
+
+std::unique_ptr<char[]> read_flatbuffer(std::ifstream& in)
+{
+  auto size = read_flatbuffer_size(in);
+  std::unique_ptr<char[]> buffer(new char[size]);
+  in.read(buffer.get(), size);
+  return buffer;
+}
+
+void verify_profile_header(std::ifstream& in)
+{
+  auto fb_ptr = read_flatbuffer(in);
+  auto header = flatbuffers::GetRoot<spark_rapids_jni::profiler::ProfileHeader>(fb_ptr.get());
+  auto magic = header->magic();
+  if (magic == nullptr) {
+    throw std::runtime_error("does not appear to be a spark-rapids profile");
+  }
+  if (magic->str() != "spark-rapids profile") {
+    std::ostringstream oss;
+    oss << "bad profile magic, expected 'spark-rapids profile' found '" << magic->str() << "'";
+    throw std::runtime_error(oss.str());
+  }
+  auto version = header->version();
+  if (version != 1) {
+    std::ostringstream oss;
+    oss << "unsupported profile version: " << version;
+    throw std::runtime_error(oss.str());
+  }
+}
+
+void convert_to_nsys_rep(std::string_view const& in_file, std::string const& out_file)
+{
+  std::ifstream in(std::string(in_file), std::ios_base::binary | std::ios_base::in);
+  verify_profile_header(in);
+
+}
+
 int main(int argc, char* argv[])
 {
-  std::string_view prog_name(argv[0]);
+  constexpr int RESULT_SUCCESS = 0;
+  constexpr int RESULT_FAILURE = 1;
+  constexpr int RESULT_USAGE = 2;
   program_options opts;
   std::vector<std::string_view> files;
   if (argc < 2) {
-    print_usage(prog_name);
-    return MAIN_RESULT_USAGE;
+    print_usage();
+    return RESULT_USAGE;
   }
   std::vector<std::string_view> args(argv + 1, argv + argc);
   try {
     auto [ options, inputs ] = parse_options(args);
     opts = options;
     files = inputs;
-  } catch (std::exception const &e) {
-    std::cerr << prog_name << ": " << e.what() << std::endl;
-    print_usage(prog_name);
-    return MAIN_RESULT_USAGE;
+  } catch (std::exception const& e) {
+    std::cerr << "spark_rapids_profile_converter: " << e.what() << std::endl;
+    print_usage();
+    return RESULT_USAGE;
   }
   if (opts.help || files.size() != 1) {
-    print_usage(prog_name);
-    return MAIN_RESULT_USAGE;
+    print_usage();
+    return RESULT_USAGE;
   }
+  auto input_file = files.front();
   std::string output_path(opts.output_path ? std::string(opts.output_path.value())
-    : std::string(files.front()) + ".nsys-rep");
-  std::cout << "Processing " << files.front() << " into " << output_path << std::endl;
-  return MAIN_RESULT_SUCCESS;
+    : std::string(input_file) + ".nsys-rep");
+  try {
+    convert_to_nsys_rep(input_file, output_path);
+  } catch (std::exception const& e) {
+    std::cerr << "Error converting " << input_file << ": " << e.what() << std::endl;
+    return RESULT_FAILURE;
+  }
+  return RESULT_SUCCESS;
 }
